4649c364f1b5665d5366df192c89924970143267
zuhairtamer-ctrl <3.26936312e+08+zuhairtamer-ctrl@users.noreply.github.com>
2026-09-14 22:15:08 +0000

    Deploy site to GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -11,6 +11,9 @@ const __dirname = path.dirname(__filename);
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(), viteSingleFile()],
+  server: {
+    allowedHosts: true,
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
